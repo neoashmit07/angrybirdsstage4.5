2f60a77cbd7ea80abaf7c55b5776f172c732fb44
neoashmit07 <neoashmit07@gmail.com>
2020-12-02 16:40:55 +0530

    added slingshot and rubberband

--- Slingshot.js
+++ Slingshot.js
@@ -7,6 +7,9 @@ class SlingShot{
             length: 10
         }
         this.pointB = pointB
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +19,40 @@ class SlingShot{
     }
 
     display(){
+        //displaying the wooden catapult
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+           
+            stroke(48,22,8)
+
+            if(pointA.x<220){
+                strokeWeight(7);
+                //rubberband
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y)
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3)
+    
+                //block for bird to rest 
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30)    
+
+            }
+            else{
+                strokeWeight(3);
+                //rubberband
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y)
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3)
+    
+                //block for bird to rest 
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30)    
+
+            }
+           
+            pop();
+            
         }
     }
     
